83f0e20db88dc9e2260f816e216e57c011214acf
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-10 09:27:27 -0800

    removing fails_with

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -5,11 +5,6 @@ class Pcre < Formula
   url 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.32.tar.bz2'
   sha256 'a913fb9bd058ef380a2d91847c3c23fcf98e92dc3b47cd08a53c021c5cde0f55'
 
-  fails_with :llvm do
-    build 2326
-    cause "Bus error in ld on SL 10.6.4"
-  end
-
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
