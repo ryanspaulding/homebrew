32a07e80174f41adb84ed195c60e5975bd86417e
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-10 09:19:54 -0800

    pcre changes got lost somehow

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -1,18 +1,22 @@
 require 'formula'
 
-class Expat <Formula
-  url 'http://downloads.sourceforge.net/project/expat/expat/2.0.1/expat-2.0.1.tar.gz'
-  homepage 'http://expat.sourceforge.net/'
-  md5 'ee8b492592568805593f81f8cdf2a04c'
+class Pcre < Formula
+  homepage 'http://www.pcre.org/'
+  url 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.32.tar.bz2'
+  sha256 'a913fb9bd058ef380a2d91847c3c23fcf98e92dc3b47cd08a53c021c5cde0f55'
 
-  def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}"
-    system "make install"
+  fails_with :llvm do
+    build 2326
+    cause "Bus error in ld on SL 10.6.4"
   end
 
-  def caveats
-    "Note that OS X has Expat 1.5 installed in /usr already."
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-utf8",
+                          "--enable-unicode-properties",
+                          "--enable-pcregrep-libz",
+    system "make test"
+    system "make install"
   end
 end
